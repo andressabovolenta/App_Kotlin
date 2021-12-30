b00435733a2a5b01e46929ec87fa0d9ecd0742ed
Andress Bovolenta <andressa.bbovolenta@gmail.com>
2021-12-30 19:49:56 -0300

    Exemplifies flow control with conditionals

---
+++ src/Cond.kt
@@ -0,0 +1,33 @@
+fun main(args : Array<String>) {
+    val num1 : Int = 10
+    val num2 : Int = 20
+
+    /*val max : Int = if(num1 < num2) num2 else num1
+    println(max)*/
+//- - - - - - - - - -//
+    /*val max : Int = if(num1 < num2) {
+        println("Block 2:")
+        num2
+    } else {
+        println("Block 1:")
+        num1
+    }
+//- - - - - - - - - -//
+    println(max)*/
+
+    /*if (num1 > num2) {
+        println("Number 1 is more than Number 2.")
+    } else {
+        println("Number 1 is less than Number 2 ")
+    }*/
+
+//- - - - - - - - - -//
+
+    /*when(num1) {
+        1 -> println("1")
+        10 -> println("10")
+        else -> {
+            println("Not available")
+        }
+    }*/
+}
