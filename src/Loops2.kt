a3d43bdfd5264d168244d99cc36509a0b0c6d79e
Andress Bovolenta <andressa.bbovolenta@gmail.com>
2021-12-30 19:53:28 -0300

    Finishes flow control with loops and presents mutable variables

---
+++ src/Loops2.kt
@@ -0,0 +1,15 @@
+fun main(args : Array<String>) {
+    var n: Int = 0
+
+     /*while (n <= 10) {
+         println(n)
+         n++
+     }*/
+
+    //- - - - - - - - - -//
+
+    /*do {
+        println(n)
+        n++
+    } while (n <= 10)*/
+}
