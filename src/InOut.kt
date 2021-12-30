98050f528f6e73578c02bd797266ff93a3759a4e
Andress Bovolenta <andressa.bbovolenta@gmail.com>
2021-12-30 19:56:01 -0300

    Address input and output data and string template

---
+++ src/InOut.kt
@@ -0,0 +1,37 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    /*val name: String
+    val age: Int
+
+    print("Enter your name: ")
+    name = readLine()!!
+    println("Hello, " + name + "!")
+    println("How old are you?")
+    age = readLine()!!.toInt()
+    println("You are " + age + " years old.")*/
+//- - - - - - - - - -//
+    /*print("Enter your name: ")
+    val name: String = readLine()!!
+    println("How old are you?")
+    val age: Int = readLine()!!.toInt()
+    println("Hi $name! You are $age years old.")
+    println("You have ${name.length} chars in your name.")*/
+
+    //- - - - - - - - - -//
+
+ /*   val name: String
+    val inputName = Scanner(System.`in`)
+    print("Enter your name: ")
+    name = inputName.next()
+//    name = readLine()!!
+
+    val age: Int
+    val inputAge = Scanner(System.`in`)
+    println("How old are you?")
+    age = inputAge.nextInt()
+//     age = readLine()!!.toInt()
+
+    println("Hi $name! You are $age years old.")
+    println("You have ${name.length} chars in your name.")*/
+}
